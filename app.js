cfb5453c7e725c33420709bf6e02a6be7ef64d9f
reydelo <reyna.deloge@gmail.com>
2015-07-16 10:33:08 -0600

    add a new route for form post action

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/form', form);
+app.use('/create', form);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/form.js
+++ routes/form.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res) {
   res.render('form', { title: 'My form title' });
 });
 
+/* Post form */
+router.post('/', function(req, res) {
+  console.log(req.body.comment);
+  res.redirect('form');
+});
+
 module.exports = router;
