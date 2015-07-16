11e9c18ae5986943ebb578105e5cf5b10fb8930e
reydelo <reyna.deloge@gmail.com>
2015-07-16 10:00:46 -0600

    create new route form

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var form = require('./routes/form');
 
 var app = express();
 
@@ -22,8 +23,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use('/ENDPOINT', VARIABLE_NAME);
 app.use('/', routes);
 app.use('/users', users);
+app.use('/form', form);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
